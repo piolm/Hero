5d1b1add1a66eed5f47ea071b140bc6783b1b92a
piolm <piolm.p.r.o16@gmail.com>
2022-05-26 15:08:37 +0300

    h

--- src/com/company/Hero.java
+++ src/com/company/Hero.java
@@ -69,4 +69,28 @@ class Location {
         this.y = y;
         this.z = z;
     }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
 }
